c4eaf5dacbcffa0b5c9a10ebb32113bb85267e4b
Steven Eubank <eubank.steven88@gmail.com>
2023-03-28 14:53:41 +0200

    Update config.ts

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,4 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
-// const sentryDSN = __SENTRY_DSN__ || ""
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000",
+const sentryDSN = __SENTRY_DSN__ || ""
 
-export { medusaUrl }
+export { sentryDSN, medusaUrl }
