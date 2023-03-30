490f0c64a5d58e0a84d0c9da7688b55308d85ff1
Steven Eubank <eubank.steven88@gmail.com>
2023-03-30 13:29:59 +0200

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,9 +7,12 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      <button onClick={() => methodDoesNotExist()}>Break the world</button>
+    </p>
   </Layout>
 )
 
-return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+// return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
 
 export default NotFoundPage
